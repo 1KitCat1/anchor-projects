b973e118b8ba2f5edbbdb817e376d910aa48bac1
1KitCat1 <denischool119@gmail.com>
2025-08-03 17:59:26 +0300

    Add init and modify expense

--- expense-tracker/programs/expense-tracker/src/lib.rs
+++ expense-tracker/programs/expense-tracker/src/lib.rs
@@ -6,11 +6,20 @@ declare_id!("92hLvpULRPnRbVCKZgA7Ft9VbaSNwEvCrTauJsbBQW3V");
 pub mod expense_tracker {    
     use super::*;    
 
-    pub fn initialize_expense(ctx: Context<InitializeExpense>) -> Result<()> {
+    pub fn initialize_expense(ctx: Context<InitializeExpense>, id: u64, amount: u64, merchant_name: String) -> Result<()> {
+
+        let expense_account = &mut ctx.accounts.expense_account;
+        expense_account.id = id;
+        expense_account.owner = ctx.accounts.authority.key();
+        expense_account.amount = amount;
+        expense_account.merchant_name = merchant_name;
         Ok(())
     }
 
-    pub fn modify_expense(ctx: Context<ModifyExpense>) -> Result<()> {
+    pub fn modify_expense(ctx: Context<ModifyExpense>, amount: u64, merchant_name: String) -> Result<()> {
+        let expense_account = &mut ctx.accounts.expense_account;
+        expense_account.amount = amount;
+        expense_account.merchant_name = merchant_name;
         Ok(())
     }
 
@@ -20,18 +29,43 @@ pub mod expense_tracker {
 }
 
 #[derive(Accounts)]
+#[instruction(id: u64)]
 pub struct InitializeExpense<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        init, 
+        payer = authority, 
+        space = 8 + 8 + 32 + 8 + 100,
+        seeds = [b"expense_account", authority.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub expense_account: Account<'info, ExpenseAccount>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
+#[instruction(id: u64)]
 pub struct ModifyExpense<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"expense_account", authority.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub expense_account: Account<'info, ExpenseAccount>,
 }
 
 #[derive(Accounts)]
-pub struct DeleteExpense<'info> {
+pub struct DeleteExpense {
+    
 }
 
 #[account]
 pub struct ExpenseAccount {
-    pub some_data: u64,
+    pub id: u64,
+    pub owner: Pubkey,
+    pub amount: u64,
+    pub merchant_name: String,
 }
